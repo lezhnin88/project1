81fcd2454f7e7bac0eca6df88db58693d352e368
lezhnin <lezhnin88@yandex.ru>
2023-06-12 16:08:05 +0500

    less1

--- script.js
+++ script.js
@@ -1,1 +1,22 @@
 'use strict'
+
+let money = prompt("Ваш бюджет на месяц?", ''),
+    time = prompt("Введите дату в формате YYYY-MM-DD", '');
+
+let appData = {
+    buget: money,
+    expenses: {},
+    income: [],
+    timeData: time,
+    saving: false
+};
+
+let a1 = prompt("Введите обязательную статью расходов в этом месяце", ''),
+    a2 = prompt("Во сколько обойдется?", ''),
+    a3 = prompt("Введите обязательную статью расходов в этом месяце", ''),
+    a4 = prompt("Во сколько обойдется?", '');
+
+appData.expenses.a1 = a2;
+appData.expenses.a3 = a4;
+
+alert(appData.buget / 30);
